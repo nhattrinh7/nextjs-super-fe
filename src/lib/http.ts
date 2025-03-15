dd7d3bd2bfe769b672f1c4a83231340cb63ea7ab
Trinh Nhat <trinhminhnhatym@gmail.com>
2025-03-16 04:32:40 +0700

    xong bai 20

---
+++ src/lib/http.ts
@@ -0,0 +1,196 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import envConfig from '@/config'
+import { normalizePath } from '@/lib/utils'
+import { LoginResType } from '@/schemaValidations/auth.schema'
+import { redirect } from 'next/navigation'
+
+type CustomOptions = Omit<RequestInit, 'method'> & {
+  baseUrl?: string | undefined
+}
+
+const ENTITY_ERROR_STATUS = 422
+const AUTHENTICATION_ERROR_STATUS = 401
+
+type EntityErrorPayload = {
+  message: string
+  errors: {
+    field: string
+    message: string
+  }[]
+}
+
+export class HttpError extends Error {
+  status: number
+  payload: {
+    message: string
+    [key: string]: any
+  }
+  constructor({ status, payload, message ='Lỗi http' }: { status: number; payload: any, message?: string }) {
+    super(message)
+    this.status = status
+    this.payload = payload
+  }
+}
+
+export class EntityError extends HttpError {
+  status: typeof ENTITY_ERROR_STATUS
+  payload: EntityErrorPayload
+  constructor({
+    status,
+    payload
+  }: {
+    status: typeof ENTITY_ERROR_STATUS
+    payload: EntityErrorPayload
+  }) {
+    super({ status, payload, message: 'Lỗi thực thể' })
+    this.status = status
+    this.payload = payload
+  }
+}
+
+let clientLogoutRequest: null | Promise<any> = null
+
+const isClient = typeof window !== 'undefined'
+
+const request = async <Response>(
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+  url: string,
+  options?: CustomOptions | undefined
+) => {
+  let body: FormData | string | undefined = undefined
+
+  if (options?.body instanceof FormData) {
+    body = options.body
+  } else if (options?.body) {
+    body = JSON.stringify(options.body)
+  }
+  
+  const baseHeaders: {[key: string]: string} =
+    body instanceof FormData
+      ? {}
+      : { 'Content-Type': 'application/json' }
+
+  if (isClient) {
+    const accessToken = localStorage.getItem('accessToken')
+    if (accessToken) {
+      baseHeaders.Authorization = `Bearer ${accessToken}`
+    }
+  }
+  // Nếu không truyền baseUrl (hoặc baseUrl = undefined) thì lấy từ envConfig.NEXT_PUBLIC_API_ENDPOINT
+  // Nếu truyền baseUrl thì lấy giá trị truyền vào, truyền vào '' thì đồng nghĩa với việc chúng ta gọi API đến Next.js Server
+
+  const baseUrl =
+    options?.baseUrl === undefined
+      ? envConfig.NEXT_PUBLIC_API_ENDPOINT
+      : options.baseUrl
+
+  const fullUrl = `${baseUrl}${normalizePath(url)}`
+
+  const res = await fetch(fullUrl, {
+    ...options,
+    headers: {
+      ...baseHeaders,
+      ...options?.headers
+    } as any,
+    body,
+    method
+  })
+  const payload: Response = await res.json()
+  const data = {
+    status: res.status,
+    payload
+  }
+  // Interceptor là nời chúng ta xử lý request và response trước khi trả về cho phía component
+  if (!res.ok) {
+    // Bị lỗi thì có thể là lỗi (EntityError) hoặc
+    if (res.status === ENTITY_ERROR_STATUS) {
+      throw new EntityError(
+        data as {
+          status: 422
+          payload: EntityErrorPayload
+        }
+      )
+    // Hoặc lỗi authentication
+    } else if (res.status === AUTHENTICATION_ERROR_STATUS) {
+      // Nếu lỗi authentication  ở client thì bắt người dùng logout ra, nhớ xóa access và refreshToken
+      if (isClient) {
+        if (!clientLogoutRequest) {
+          clientLogoutRequest = fetch('/api/auth/logout', {
+            method: 'POST',
+            body: null, // Logout sẽ cho phép luôn thành công dù cho accessToken hết hạn, vẫn logout được, ta sẽ xóa token trong cookie và localStorage là dc
+            headers: {
+              ...baseHeaders
+            } as any
+          })
+          try {
+            await clientLogoutRequest
+          } catch (error) {
+          } finally {
+            localStorage.removeItem('accessToken')
+            localStorage.removeItem('refreshToken')
+            clientLogoutRequest = null
+            // Redirect về trang Login có thể bị loop vô hạn nếu ở trang Login mình gọi các API cần accessToken
+            location.href = '/login'
+          }
+        }
+      } else {
+        const accessToken = (options?.headers as any)?.Authorization.split(
+          'Bearer '
+        )[1]
+        redirect(`/logout?accessToken=${accessToken}`)
+      }
+    } else {
+      throw new HttpError(data)
+    }
+  }
+  // Đảm bảo logic dưới đây chỉ chạy ở phía client (browser)
+  if (isClient) {
+    const normalizeUrl = normalizePath(url)
+    if (
+      // ['auth/login', 'auth/register'].some(
+      //   (item) => item === normalizePath(url)
+      // )
+      normalizeUrl === 'api/auth/login'
+    ) {
+      const { accessToken, refreshToken } = (payload as LoginResType).data
+      localStorage.setItem('accessToken', accessToken)
+      localStorage.setItem('refreshToken', refreshToken)
+    } else if (normalizeUrl === 'api/auth/logout') {
+      localStorage.removeItem('accessToken')
+      localStorage.removeItem('refreshToken')
+    }
+  }
+  return data
+}
+
+const http = {
+  get<Response>(
+    url: string,
+    options?: Omit<CustomOptions, 'body'> | undefined
+  ) {
+    return request<Response>('GET', url, options)
+  },
+  post<Response>(
+    url: string,
+    body: any,
+    options?: Omit<CustomOptions, 'body'> | undefined
+  ) {
+    return request<Response>('POST', url, { ...options, body })
+  },
+  put<Response>(
+    url: string,
+    body: any,
+    options?: Omit<CustomOptions, 'body'> | undefined
+  ) {
+    return request<Response>('PUT', url, { ...options, body })
+  },
+  delete<Response>(
+    url: string,
+    options?: Omit<CustomOptions, 'body'> | undefined
+  ) {
+    return request<Response>('DELETE', url, { ...options })
+  }
+}
+
+export default http
